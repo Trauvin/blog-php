5776a501768f85505911e3b16c3fbef19207fe84
Trauvin <mateusgalote@gmail.com>
2020-04-11 21:26:21 -0300

    second commit

---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+<div class="footer">
+    <div class="footer-content">
+      <div class="footer-section about">
+        <h1 class="logo-text">Math<span>On</span></h1>
+        <p>
+          Math blog é um blog de assuntos diversos. Sinta-se a vontade para escrever o que bem entender.
+          
+        </p>
+        <!-- <br> -->
+
+        <div class="contact">
+          <i class="fa fa-phone"> &nbsp; 027 997507403</i>
+          <i class="fa fa-envelope"> &nbsp; mateusgalote@gmail.com</i>
+        </div>
+
+        <div class="social">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-youtube-play"></i></a>
+        </div>
+
+      </div>
+
+      <div class="footer-section quick-links">
+        <h2>QUICK LINKS</h2>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Contact</li>
+          </a>
+          <a href="#">
+            <li>Mentors</li>
+          </a>
+          <a href="#">
+            <li>Galleries</li>
+          </a>
+          <a href="#">
+            <li>Write for us</li>
+          </a>
+          <a href="#">
+            <li>Terms and conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form-div">
+        <h2>Contact Us</h2>
+        <br>
+        <form action="index.php" method="post">
+          <input type="text" name="email-address" class="text-input contact-input" placeholder="Your email address">
+          <textarea name="message" cols="30" rows="3" class="text-input contact-input" placeholder="Message..."></textarea>
+          <button type="submit" name="send-msg-btn" class="send-msg-btn">
+            <i class="fa fa-send"></i> Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      <p>© Math</p>
+    </div>
+  </div>
